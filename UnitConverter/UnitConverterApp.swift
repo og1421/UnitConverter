1593839ba67533feefc660e57ea52448adf8c7b5
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-28 18:37:23 -0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Orlando Moraes Martins on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
